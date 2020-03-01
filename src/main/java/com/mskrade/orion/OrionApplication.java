5fbc5f7e9ebfa70b4e7604e1aaf0dda4bede53fa
mskrade <michael@skrade.com>
2020-02-29 21:57:27 -0600

    Added cache to avoid multiple public API calls for the same set.

--- src/main/java/com/mskrade/orion/OrionApplication.java
+++ src/main/java/com/mskrade/orion/OrionApplication.java
@@ -1,9 +1,13 @@
 package com.mskrade.orion;
 
+import com.mskrade.orion.models.Card;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
 
 @SpringBootApplication
 public class OrionApplication {
@@ -12,6 +16,9 @@ public class OrionApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
+	@Bean
+	public HashMap<String, List<Card>> hashMap() {return new HashMap<>();}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrionApplication.class, args);
--- src/main/java/com/mskrade/orion/services/ScryfallService.java
+++ src/main/java/com/mskrade/orion/services/ScryfallService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,9 +20,17 @@ public class ScryfallService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private HashMap<String, List<Card>> cardCache;
+
     public List<Card> getCardList(String set) {
-        CardList initialList = restTemplate.getForObject("https://api.scryfall.com/cards/search?q=e:" + set, CardList.class);
-        return initialList.getData();
+        if (cardCache.containsKey(set)) {
+            return cardCache.get(set);
+        } else {
+            CardList initialList = restTemplate.getForObject("https://api.scryfall.com/cards/search?q=e:" + set, CardList.class);
+            cardCache.put(set, initialList.getData());
+            return initialList.getData();
+        }
     }
 
     public List<Set> getSetList() {
